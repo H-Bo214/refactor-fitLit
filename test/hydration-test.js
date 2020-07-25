f573f99299e30f62d55b3117b8242c8defb7b11e
Jake West <jacobkwest93@gmail.com>
2020-07-25 12:29:29 -0600

    Add tests for Hydration

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -13,4 +13,23 @@ describe.only('Hydration', function() {
     });
   });
 
+    it('should be a function', function() {
+      expect(Hydration).to.be.a('function');
+    });
+
+    it('should be an instance of Sleep', function() {
+      expect(hydration1).to.be.an.instanceof(Hydration);
+    });
+
+    it('should have an id', function() {
+      expect(hydration1.userID).to.equal(1);
+    })
+
+    it('should have a date', function() {
+      expect(hydration1.date).to.equal("2019/06/15");
+    })
+
+    it('should have a number of ounces consumed', function () {
+      expect(hydration1.numOunces).to.equal(37);
+    })
 })
