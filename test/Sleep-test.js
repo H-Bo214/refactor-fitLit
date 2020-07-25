dbad1722fb727cf217998fb3548d47882b046475
Jake West <jacobkwest93@gmail.com>
2020-07-25 09:43:46 -0600

    Add tests for Sleep class

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 
 import Sleep from '../src/Sleep'
 
-describe('Sleep', function() {
+describe.only('Sleep', function() {
   let sleep1;
 
   beforeEach(function() {
@@ -13,4 +13,28 @@ describe('Sleep', function() {
       "sleepQuality": 2.2
     });
   });
+
+  it('should be a function', function() {
+    expect(Sleep).to.be.a('function');
+  });
+
+  it('should be an instance of Sleep', function() {
+    expect(Sleep).to.be.an.instanceof(Sleep);
+  });
+
+  it('should have an id', function() {
+    expect(sleep1.userID).to.equal(1);
+  })
+
+  it('should have a date', function() {
+    expect(sleep1.date).to.equal("2019/06/15");
+  })
+
+  it('should have a number of hours slept', function () {
+    expect(sleep1.hoursSlept).to.equal(6.1);
+  })
+
+  it('should have have a number for sleep quality', function() {
+    expect(sleep1.sleepQuality).to.equal(2.2);
+  })
 })
