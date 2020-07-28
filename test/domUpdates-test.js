a9d9059b23a05e537f9fa1ffb5a0337dc55822a6
Jake West <jacobkwest93@gmail.com>
2020-07-28 11:43:26 -0600

    Add test for displayUserInfo()

--- test/domUpdates-test.js
+++ test/domUpdates-test.js
@@ -3,6 +3,8 @@ const expect = chai.expect;
 const spies = require('chai-spies');
 import domUpdates from '../src/domUpdates';
 import User from '../src/User'
+import UserRepo from '../src/User-repo'
+
 chai.use(spies);
 
 describe.only('domUpdates', function() {
@@ -14,15 +16,14 @@ describe.only('domUpdates', function() {
       "email": "FakeEmail@hotmail.com",
       "strideLength": 4.3,
       "dailyStepGoal": 10000,
-      "friends": [
-        16,
-        4,
-        8
-      ]
+      "friends": []
     });
     global.document = {};
     chai.spy.on(document, ['getElementById', 'querySelector'], () => {
-      return { innerText: '' }
+      return {
+        innerText: '',
+        insertAdjacentHTML: () => {}
+      }
     });
   });
 
@@ -32,4 +33,15 @@ describe.only('domUpdates', function() {
     expect(document.getElementById).to.have.been.called(1)
     expect(document.getElementById).to.have.been.called.with('headerText')
   });
+
+  it('should spy on displayUserInfo', function() {
+    domUpdates.displayUserInfo();
+
+    expect(document.querySelector).to.have.been.called(1)
+    expect(document.querySelector).to.have.been.called.with('.sidebar-header-name')
+    expect(document.getElementById).to.have.been.called(3)
+    expect(document.getElementById).to.have.been.called.with('userAddress')
+    expect(document.getElementById).to.have.been.called.with('userEmail')
+    expect(document.getElementById).to.have.been.called.with('friendList')
+  })
 })
