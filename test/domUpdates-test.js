38ba54864fac423a0a86bd94095eb7e65a637091
Jake West <jacobkwest93@gmail.com>
2020-07-27 19:26:28 -0600

    Add test with spy for displayHeader(); needs to be refactored after checkin with Will

--- test/domUpdates-test.js
+++ test/domUpdates-test.js
@@ -1,5 +1,23 @@
 const chai = require('chai');
 const expect = chai.expect;
 const spies = require('chai-spies');
-const domUpdates = require('..src/domUpdates')
+import domUpdates from '../src/domUpdates';
 chai.use(spies);
+
+describe.only('domUpdates', function() {
+  beforeEach(() => {
+    global.document = {
+      getElementById: () => {
+        innerText: () => {}
+      }
+    };
+    chai.spy.on(document, ['getElementById', 'querySelector']);
+  });
+
+  it('should spy on displayHeader', function() {
+    domUpdates.displayHeader();
+
+    expect(document.getElementById).to.have.been.called(1)
+    expect(document.getElementById).to.have.been.called.with('headerText')
+  });
+})
