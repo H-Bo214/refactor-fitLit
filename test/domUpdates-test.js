d47e8c62bf1ccd198ddeb09d9586ff54962eb5ec
Jake West <jacobkwest93@gmail.com>
2020-07-28 11:31:11 -0600

    Refactor beforeEach statement for displayHeader test #40

--- test/domUpdates-test.js
+++ test/domUpdates-test.js
@@ -2,16 +2,28 @@ const chai = require('chai');
 const expect = chai.expect;
 const spies = require('chai-spies');
 import domUpdates from '../src/domUpdates';
+import User from '../src/User'
 chai.use(spies);
 
 describe.only('domUpdates', function() {
   beforeEach(() => {
-    global.document = {
-      getElementById: () => {
-        innerText: () => {}
-      }
-    };
-    chai.spy.on(document, ['getElementById', 'querySelector']);
+    domUpdates.user = new User({
+      "id": 999,
+      "name": "Fake Data",
+      "address": "1500 Fake Address, Faketown VA",
+      "email": "FakeEmail@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    });
+    global.document = {};
+    chai.spy.on(document, ['getElementById', 'querySelector'], () => {
+      return { innerText: '' }
+    });
   });
 
   it('should spy on displayHeader', function() {
